57277f704cb2d73938a198de0abbbb24c5531f56
fox <mk.alex1989@gmail.com>
2016-07-04 21:23:32 +0200

    Creation fichier stockage tableur

---
+++ reporting_excl.py
@@ -0,0 +1,6 @@
+#!/usr/local/bin/python3.3
+
+### surnom : Fox
+### date : 9 Janvier 2016
+### programme : reporting_excl.py
+### objectif : création des fonctions de stockage des donnees de sites sur excel
