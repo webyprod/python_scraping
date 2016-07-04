2c3b72730041b35b49ecc2577d08d9fb729bb2f5
fox <mk.alex1989@gmail.com>
2016-07-04 21:07:43 +0200

    Creation fichier Scraping BS4

---
+++ scraping_site.py
@@ -0,0 +1,7 @@
+#!/usr/local/bin/python3.3
+
+### surnom : Fox
+### date : 9 Janvier 2016
+### programme : scraping_site.py
+### objectif : retirer toutes les balises nécessaires pour le référenceur
+###            pour identifier un site
