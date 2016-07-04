769b54e922120cc727c5452045283e840759c886
fox <mk.alex1989@gmail.com>
2016-07-04 21:25:30 +0200

    Cette fonction vérifie sur quelle ligne nous allons stocker les infos du site

--- reporting_excl.py
+++ reporting_excl.py
@@ -4,3 +4,19 @@
 ### date : 9 Janvier 2016
 ### programme : reporting_excl.py
 ### objectif : création des fonctions de stockage des donnees de sites sur excel
+
+
+def verif_niveau():
+  '''Cette fonction a pour but de vérifier quelle ligne est vide '''
+  ligne = 0
+  for i in range(2,10000):
+    liste = []
+    for j in range(1,7):
+      valeur = classeur.cell(row=i,column=j).value
+      if valeur == None:
+        continue
+      liste.append(valeur)
+    if liste == []:
+      ligne = i
+      break
+  return ligne
